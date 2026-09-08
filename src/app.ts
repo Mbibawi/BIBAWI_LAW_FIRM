41a7c66005f25ec4d083f779bbd33cf76e30bb6c
Mina BIBAWI <tv.share1306@gmail.com>
2026-09-08 11:28:52 +0200

    v0.2.3

--- src/app.ts
+++ src/app.ts
@@ -237,7 +237,11 @@ function HomePage(router: Router): HTMLElement {
 
   /* Hero */
   //const hero = el('section', ['sec', 'hero'], '', { style: `background:linear-gradient(rgba(8,23,38,.55),rgba(8,23,38,.72)),url(${IMG}photo-1589578527966-fdac0f44566c?auto=format&fit=crop&w=1800&q=80) center/cover no-repeat` });
-  const hero = el('section', ['sec', 'hero'], '', { style: `background:linear-gradient(rgba(8,23,38,.55),rgba(8,23,38,.72)),url(${IMAGES}hero.png?auto=format&fit=crop&w=1800&q=80) center/cover no-repeat` });
+
+  //const hero = el('section', ['sec', 'hero'], '', { style: `background:linear-gradient(rgba(7, 52, 61, 0.55),rgba(8,23,38,.72)),url(${IMAGES}hero.png?auto=format&fit=crop&w=1800&q=80) center/cover no-repeat` });
+  const hero = el('section', ['sec', 'hero'], '', {
+    style: `background-image: linear-gradient(rgba(7, 52, 61, 0.55), rgba(8, 23, 38, 0.72)), url('${IMAGES}hero.png')`
+  });
   const heroInner = el('div', 'w');
   const heroBox = el('div', '', '', { style: 'maxWidth:640px' });
   heroBox.appendChild(el('p', 'sub', 'Cabinet d\u2019avocat \u2014 Droit des affaires', { style: 'color:#e7ecf1' }));
